c7add0ac4380730a8b49d679e48ad5f42fcc3f55
bzhang2040 <BrentCopeland2040@gmail.com>
2025-10-19 15:41:58 -0500

    Write all the animations out in linear order

--- Shaders/Animations2.glsl
+++ Shaders/Animations2.glsl
@@ -11,49 +11,47 @@
     Yaw(89.999, -100, -99, temp);
     CameraHeight(WATER_HEIGHT+10.1, -100, -99, temp);
     
+    
+    Yaw(    0.0, 36.9, 61, EaseOutSin(EaseInOutSin(temp)));
+    Water(  1.0, 49, 60, temp);
+    Roll(   1.0, 60, 84, EaseInOutSin(temp));
+    Fisheye(0.0, 97, 145, temp);
     CameraHeight(WATER_HEIGHT+5.0, 37, 50, EaseInOutSin(temp));
     CameraHeight(trackPos.y+2, 74, 96, EaseInOutSin(temp));
     
-    Water(1.0, 49, 60, temp);
-    Roll(1.0, 60, 84, EaseInOutSin(temp));
-    Yaw(0.0, 36.9, 61, EaseOutSin(EaseInOutSin(temp)));
+    Speed(500.0, 160, 160.1);
+    Speed( 80.0, 169, 169.1);
     
-    Fisheye(0.0, 97, 145, temp);
-    
-    Shutter(0.5, 313-6, 313, temp);
-    Fov(  120.0, 313-6, 313, tan(temp*3.14159/2.0/2.0));
+    Speed( 160.0, 265, 275);
+    Acid(    0.6, 271, 275, powf(0.6, cubesmooth(temp)));
+    Acid(    0.2, 265, 271, powf(0.75, temp));
+    Acid(    0.8, 277, 313, temp);
+    Shutter( 0.5, 307, 313, temp);
+    Fov(   120.0, 307, 313, tan(temp*3.14159/2.0/2.0));
+    SunAngle(175, 308, 505, temp);
     
     //  first chorus beat =  313
-    // second chorus beat =  721
-    //  third chorus beat = 1079
     
-    Shutter(1.0, 360, 360+6, temp);
-    Shutter(0.5, 1076-6, 1079, temp);
+    Shutter(1.0, 360, 366, temp);
+    Fov(  110.0, 360, 363, tan(temp*3.14159/2.0/2.0));
+    Speed(120.0, 361, 366);
     
-    Acid(0.2, 265, 271, powf(0.75, temp));
-    Acid(0.6, 271, 275, powf(0.6, cubesmooth(temp)));
-    Acid(0.8, 277, 313, temp);
     Acid(0.0, 500, 505, powf(4.0, temp));
-    
-    Fisheye(1.0, 600, 630, temp + 0.0*cubesmooth(tan(temp * 3.14159 / 4.0)));
-    Fisheye(0.0, 700, 750, temp);
-    
-    SunAngle(175, 308, 505, temp);
     SunAngle(187, 505, 529, temp);
     SunAngle(354, 529, 673, temp);
+    
+    Fisheye( 1.0, 600, 630, temp + 0.0*cubesmooth(tan(temp * 3.14159 / 4.0)));
     SunAngle(380, 673, 721, temp);
+    Fisheye( 0.0, 700, 750, temp);
     
-    Fov(110.0,   360, 360+3, tan(temp*3.14159/2.0/2.0));
-    Fov(110.0,  1073,  1079, tan(temp*3.14159/2.0/2.0));
+    // second chorus beat =  721
     
-    Speed(500.0, 160, 160.1);
-    Speed(80.0, 169, 169.1);
-
-    Speed(160.0, 265, 275);
-    Speed(120.0, 361, 366);
-
-    Speed(500.0, 721-10, 721);
+    Speed(500.0, 711, 721);
     Speed(120.0, 913, 937);
-
+    
+    Shutter(0.5, 1070, 1079, temp);
     Speed(160.0, 1073, 1079);
+    Fov(  110.0, 1073, 1079, tan(temp*3.14159/2.0/2.0));
+    
+    //  third chorus beat = 1079
 }
